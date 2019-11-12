dced511d88c20806f8f2a14a489834bd90732bfc
Arno <arno@delandtsheer.eu>
2019-11-12 20:46:22 +0100

    almost fixed anime pics

--- Popcorn/Popcorn/Repositories/PopcornRepository.cs
+++ Popcorn/Popcorn/Repositories/PopcornRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Popcorn.Models;
 using System;
 using System.Collections.Generic;
@@ -30,12 +31,6 @@ namespace Popcorn.Repositories
         #endregion
 
         #region ConstShowApiUrl's
-        //parameters:
-        // --> page     (select which page you want to display)
-        // --> sort     (select which type of show  you specificly want to display, all possible sort parameters will be hardcoded)
-        // --> order    (select which order you want the shows displayed, possible values are 1 and -1 this will invert the list of possible items)
-        // --> genre    (select which genre of show you specificly want to display, all possible genres will be hardcoded)
-        // --> keywords (select all shows corresponding with the submited keywords, f.e 'orange' will return all movies with orange in the title)
         //full url example: https://tv-v2.api-fetch.website/shows/1?sort=Trending&order=1&genre=&keywords=The
 
         const string SHOWSPAGE = "https://tv-v2.api-fetch.website/shows/";
@@ -46,12 +41,6 @@ namespace Popcorn.Repositories
         #endregion
 
         #region ConstAnimeApiUrl's
-        //parameters:
-        // --> page     (select which page you want to display)
-        // --> sort     (select which type of anime  you specificly want to display, all possible sort parameters will be hardcoded)
-        // --> order    (select which order you want the anime's displayed, possible values are 1 and -1 this will invert the list of possible items)
-        // --> genre    (select which genre of anime you specificly want to display, all possible genres will be hardcoded)
-        // --> keywords (select all anime's corresponding with the submited keywords, f.e 'orange' will return all movies with orange in the title)
         //full url example: https://tv-v2.api-fetch.website/animes/1?sort=name&order=1&genre=&keywords=
 
         const string ANIMEPAGE = "https://tv-v2.api-fetch.website/animes/";
@@ -61,11 +50,11 @@ namespace Popcorn.Repositories
         public static readonly List<string> ANIMESORTLIST = new List<string>() { "Popularity", "Name", "Year" };
         public static readonly List<string> ANIMETYPELIST = new List<string>() { "All", "Movies", "TV", "OVA", "ONA" };
 
-        //possible site to retrieve the images from: https://myanimelist.net of https://kitsu.io/anime/one-punch-man
+        //site to retrieve the images from: https://private-anon-12aa033b85-jikan.apiary-proxy.com/v3/anime/36862/pictures
         #endregion
 
 
-        
+
         public static async Task<List<Movie>> GetMoviesAsync(string keyword, string sortby)
         {
             try
@@ -89,7 +78,7 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
@@ -117,7 +106,7 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
@@ -145,7 +134,7 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
@@ -173,7 +162,7 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
@@ -191,6 +180,11 @@ namespace Popcorn.Repositories
                 {
                     List<Anime> Animes = JsonConvert.DeserializeObject<List<Anime>>(json);
                     Debug.WriteLine("Deserialize succesfull");
+
+                    foreach (Anime a in Animes)
+                    {
+                        a.Images = await GetAnimeThubnailsAsync(a.Id);
+                    }
                     return Animes;
                 }
                 else
@@ -201,7 +195,7 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
@@ -229,7 +223,50 @@ namespace Popcorn.Repositories
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Try-Catch methode failed");
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
+                throw ex;
+            }
+        }
+
+        public static async Task<Thumbnail> GetAnimeThubnailsAsync(string id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                String url = string.Format("https://api.jikan.moe/v3/anime/{0}/pictures", id);
+                String json = await client.GetStringAsync(url);
+                Debug.WriteLine("Json: " + json);
+                if (json != null)
+                {
+                    Thumbnail pics = new Thumbnail();
+                    JObject obj = JObject.Parse(json);
+
+                    pics.Poster = (string)obj.SelectToken("pictures[0].large");
+                    try
+                    {
+                        pics.FanArt = (string)obj.SelectToken("pictures[1].large");
+                        pics.Banner = (string)obj.SelectToken("pictures[2].large");
+                    }
+                    catch (Exception ex)
+                    {
+                        pics.FanArt = "";
+                        pics.Banner = "";
+                        throw ex;
+                    }
+                    
+
+                    return pics;
+                }
+                else
+                {
+                    Debug.WriteLine("Error, no data recieved.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Try-Catch methode failed: " + ex);
                 throw ex;
             }
         }
